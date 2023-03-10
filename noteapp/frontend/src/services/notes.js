445e83bc0a14136e1b45b1dc2b41973eba5f35e6
captainkent <29131214+captainkent@users.noreply.github.com>
2023-03-10 17:30:48 +0100

    Formatting and added token stuff.

--- noteapp/frontend/src/services/notes.js
+++ noteapp/frontend/src/services/notes.js
@@ -1,23 +1,35 @@
-import axios from 'axios'
-const baseUrl = '/api/notes'
+import axios from 'axios';
+const baseURL = 'http://localhost:3001/api/notes';
+
+let token = null;
+
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`;
+};
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+  const request = axios.get(baseURL);
+  return request.then((response) => response.data);
+};
 
-const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
-}
+const create = (newObject) => {
+  const config = {
+    headers: { Authorization: token },
+  };
+
+  const request = axios.post(baseURL, newObject, config);
+  return request.then((response) => response.data);
+};
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
-}
+  const request = axios.put(baseURL + `/${id}`, newObject);
+  return request.then((response) => response.data);
+};
 
-export default { 
-  getAll, 
-  create, 
-  update 
-}
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  getAll,
+  create,
+  update,
+  setToken,
+};
