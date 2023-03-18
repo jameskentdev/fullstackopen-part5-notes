84002f264562cdc76e2558201255a445a7c3dfbf
captainkent <29131214+captainkent@users.noreply.github.com>
2023-03-18 14:51:11 +0100

    Test logs in via requests. Added test for wrong password and toggling note important status.

--- noteapp/frontend/cypress/e2e/note_app.cy.js
+++ noteapp/frontend/cypress/e2e/note_app.cy.js
@@ -1,5 +1,13 @@
 describe('Note app', function () {
   beforeEach(function () {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset');
+    const user = {
+      name: 'Teddy Kent',
+      username: 'teddy',
+      password: 'password',
+    };
+
+    cy.request('POST', 'http://localhost:3001/api/users', user);
     cy.visit('http://localhost:3000');
   });
 
@@ -23,11 +31,25 @@ describe('Note app', function () {
     cy.contains('Teddy Kent logged in');
   });
 
+  it('login fails with wrong password', function () {
+    cy.contains('login').click();
+    cy.get('#username').type('teddy');
+    cy.get('#password').type('wrong');
+    cy.get('#login-button').click();
+
+    cy.get('.error')
+      .should('contain', 'Wrong credentials')
+      .and('have.css', 'color', 'rgb(255, 0, 0)')
+      .and('have.css', 'border-style', 'solid');
+
+    cy.get('html').should('not.contain', 'Teddy Kent logged in');
+  });
+
   describe('when logged in', function () {
     beforeEach(function () {
       cy.contains('login').click();
-      cy.get('#username').type('teddy');
-      cy.get('#password').type('password');
+      cy.get('input:first').type('teddy');
+      cy.get('input:last').type('password');
       cy.get('#login-button').click();
     });
 
@@ -36,6 +58,22 @@ describe('Note app', function () {
       cy.get('#note').type('a note created by cypress');
       cy.contains('save').click();
       cy.contains('a note created by cypress');
+    });
+
+    describe('and a note exists', function () {
+      beforeEach(function () {
+        cy.contains('new note').click();
+        cy.get('#note').type('another note cypress');
+        cy.contains('save').click();
+      });
+
+      it('it can be made not important', function () {
+        cy.contains('another note cypress')
+          .contains('make not important')
+          .click();
+
+        cy.contains('another note cypress').contains('make important');
+      });
     });
   });
 });
