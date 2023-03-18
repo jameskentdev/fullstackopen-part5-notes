4be215b3a3adb454e0157641b5f79a812767c38e
captainkent <29131214+captainkent@users.noreply.github.com>
2023-03-18 14:12:50 +0100

    Added cypress e2e testing. Fixed some issues with eslint.

--- noteapp/frontend/.eslintrc.js
+++ noteapp/frontend/.eslintrc.js
@@ -4,8 +4,13 @@ module.exports = {
     browser: true,
     es6: true,
     'jest/globals': true,
+    'cypress/globals': true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:cypress/recommended',
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
